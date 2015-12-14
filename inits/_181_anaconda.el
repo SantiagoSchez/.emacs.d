97568ff546284822542798f8c736c7f57f150ec0
SanchezSobrino <sanchezsobrino@gmail.com>
2015-12-14 08:59:57 +0100

    Anaconda mode disabled

---
+++ inits/_181_anaconda.el
@@ -0,0 +1,16 @@
+;; Provides some useful utilities on python development
+;; Requires pip packages: jedi service_factory
+;; https://github.com/proofit404/anaconda-mode
+(use-package anaconda-mode
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'eldoc-mode))
+
+;; Company bindings for anaconda
+;; https://github.com/proofit404/company-anaconda
+(use-package company-anaconda
+  :ensure t
+  :ensure company
+  :config
+  (add-to-list 'company-backends 'company-anaconda))
